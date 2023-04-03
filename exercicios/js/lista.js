885c976b1ec7819341611ce06cb1c89f48690b10
nascimentodorafael <107429969+nascimentodorafael@users.noreply.github.com>
2023-04-03 14:01:02 -0300

    Add files via upload

---
+++ exercicios/js/lista.js
@@ -0,0 +1,21 @@
+let opcao = parseInt(prompt());
+
+switch(opcao){
+    case 1:
+        window.location.href = 'pages/exercicio1.html';
+        break;
+    case 2:
+        window.location.href = 'pages/exercico2.html';
+        break;
+    case 3:
+        window.location.href = 'pages/exercicio3.html';
+        break;
+    case 4:
+        window.location.href = 'pages/exercicio4.html';
+        break;
+    case 5:
+        window.location.href = 'pages/exercicio5.html';
+        break;
+    case 6:
+        window.location.href = 'pages/exercicio6.html';
+        break;
